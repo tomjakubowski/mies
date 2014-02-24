e0b4a399df8d02612014ee12e30cb2deacee4e5e
Tom Jakubowski <tom@crystae.net>
2014-02-23 22:58:42 -0800

    make status as a fork more clear

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject mies-weasel/lein-template "0.1.0-SNAPSHOT"
-  :description "A minimal ClojureScript project template with Weasel REPL."
+  :description
+  "A minimal ClojureScript project template with Weasel REPL, forked from mies."
   :url "http://github.com/tomjakubowski/mies-weasel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
