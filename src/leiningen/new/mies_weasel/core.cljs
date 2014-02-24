aa1ad1c8095ff0b24f23b0f0756d503c6ab7f25f
Tom Jakubowski <tom@crystae.net>
2014-02-23 23:37:28 -0800

    make the generated cljs print to the REPL

--- src/leiningen/new/mies_weasel/core.cljs
+++ src/leiningen/new/mies_weasel/core.cljs
@@ -1,9 +1,15 @@
 (ns {{name}}.core
   (:require [weasel.repl :as repl]))
 
-(enable-console-print!)
-
 (if (nil? @repl/ws-connection)
-  (repl/connect "ws://localhost:9001"))
+  (repl/connect "ws://localhost:9001" :verbose true))
 
-(println "Hello world!")
+;;; this should be cleaner w/ weasel 0.1.1 + callbacks...
+(defn hello []
+  (try
+    (println "Hello REPL!")
+    (catch js/Error e
+      ;; if the REPL is not yet connected, try again
+      (js/setTimeout hello 500))))
+
+(hello)
