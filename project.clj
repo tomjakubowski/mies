df14fa5e0ce0d6cda81a483718cf22b9d23cdfe1
Tom Jakubowski <tom@crystae.net>
2014-02-24 04:28:45 -0800

    cut 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-weasel/lein-template "0.1.1-SNAPSHOT"
+(defproject mies-weasel/lein-template "0.1.1"
   :description
   "A minimal ClojureScript project template with Weasel REPL, forked from mies."
   :url "http://github.com/tomjakubowski/mies-weasel"
