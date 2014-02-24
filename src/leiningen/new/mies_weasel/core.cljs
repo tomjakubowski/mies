c551a29a95e12a365e60007799340260a24a6a76
Tom Jakubowski <tom@crystae.net>
2014-02-23 22:54:29 -0800

    add weasel/piggieback to template

--- src/leiningen/new/mies_weasel/core.cljs
+++ src/leiningen/new/mies_weasel/core.cljs
@@ -1,5 +1,9 @@
-(ns {{name}}.core)
+(ns {{name}}.core
+  (:require [weasel.repl :as repl]))
 
 (enable-console-print!)
+
+(if (nil? @repl/ws-connection)
+  (repl/connect "ws://localhost:9001"))
 
 (println "Hello world!")
--- src/leiningen/new/mies_weasel/project.clj
+++ src/leiningen/new/mies_weasel/project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]]
+                 [org.clojure/clojurescript "0.0-2138"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [weasel "0.1.0"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :source-paths ["src"]
 
